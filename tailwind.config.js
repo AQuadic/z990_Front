7de59b01875e7adb5e84b3b1d50c29d681f52b44
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2024-12-23 18:08:35 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*html'],
+  theme: {
+    extend: {
+      borderImage: {
+        'gradient-linear': 'linear-gradient(269.42deg, #000000 0.47%, #FFFFFF 99.53%)',
+      },
+    },
+  },
+  plugins: [],
+};
